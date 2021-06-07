6ff66712277157ec224b08f55e77950f6567520a
Xiaohui <xiaohui@hiwin-gsv.com>
2021-06-07 13:58:20 +0800

    remove native-base

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/es/integration/react';
-import SplashScreen from 'react-native-splash-screen';
+import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/es/integration/react';
 
-import { Root, StyleProvider } from 'native-base';
 
 import Loading from './components/UI/Loading';
 import AppContainer from './components/AppContainer/AppContainer';
@@ -13,28 +11,26 @@ import AppContainer from './components/AppContainer/AppContainer';
 class App extends React.Component {
   constructor() {
     super();
-    this.state = { loading: true };
+    this.state = {loading: true};
   }
 
   async componentDidMount() {
-    this.setState({ loading: false });
+    this.setState({loading: false});
   }
 
   render() {
-    const { loading } = this.state;
-    const { store, persistor } = this.props;
+    const {loading} = this.state;
+    const {store, persistor} = this.props;
 
     if (loading) {
-      return <Loading />;
+      return <Loading/>;
     }
     return (
-      <Root>
-        <Provider store={store}>
-          <PersistGate loading={<Loading />} persistor={persistor}>
-            <AppContainer />
-          </PersistGate>
-        </Provider>
-      </Root>
+      <Provider store={store}>
+        <PersistGate loading={<Loading/>} persistor={persistor}>
+          <AppContainer/>
+        </PersistGate>
+      </Provider>
     );
   }
 }
